52f3f5527208f68837b596fa6515e8479d06614a
shevchenkobn <bohdan.shevchenko1@nure.ua>
2017-08-26 13:48:18 +0300

    database

--- PerformanceEvaluatingApp/Global.asax.cs
+++ PerformanceEvaluatingApp/Global.asax.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using PerformanceEvaluatingApp.Models;
 
 namespace PerformanceEvaluatingApp
 {
@@ -12,6 +14,8 @@ namespace PerformanceEvaluatingApp
     {
         protected void Application_Start()
         {
+            Database.SetInitializer(new CreateDatabaseIfNotExists<WebsitesContext>());
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
